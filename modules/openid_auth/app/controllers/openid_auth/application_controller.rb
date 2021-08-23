055708bd50b7798f27b238ea9cfefee8dbeac21d
Larry Crochet <larry.crochet@libertyits.com>
2021-08-23 18:17:24 -0400

    static token check adjust

--- modules/openid_auth/app/controllers/openid_auth/application_controller.rb
+++ modules/openid_auth/app/controllers/openid_auth/application_controller.rb
@@ -7,14 +7,12 @@ module OpenidAuth
     skip_before_action :set_tags_and_extra_content, raise: false
 
     def validate_user
-      unless token.client_credentials_token? || token.ssoi_token?
-        unless token.static?
-          raise Common::Exceptions::RecordNotFound, @current_user.uuid if @current_user.mpi_status == 'NOT_FOUND'
-          raise Common::Exceptions::BadGateway if @current_user.mpi_status == 'SERVER_ERROR'
+      unless token.payload['static'] || token.client_credentials_token? || token.ssoi_token?
+        raise Common::Exceptions::RecordNotFound, @current_user.uuid if @current_user.mpi_status == 'NOT_FOUND'
+        raise Common::Exceptions::BadGateway if @current_user.mpi_status == 'SERVER_ERROR'
 
-          Rails.logger.info("Logged in user with id #{@session&.uuid}, token #{obscure_token}")
-        end
-        Session.obscure_token(token.to_s)
+        obscure_token = Session.obscure_token(token.to_s)
+        Rails.logger.info("Logged in user with id #{@session&.uuid}, token #{obscure_token}")
       end
     end
 
