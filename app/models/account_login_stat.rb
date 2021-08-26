ee0ee9cf5ff992d3294d027fe8d41a86b344a3f4
Josh Scanish <josh.scanish@oddball.io>
2021-08-26 17:23:17 -0400

    Add comment

--- app/models/account_login_stat.rb
+++ app/models/account_login_stat.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# It's important that the datetime columns on this table fit a "#{name}_at" pattern
+# where the name matches one of the service_names in the SAML::User::AUTHN_CONTEXTS hash.
 class AccountLoginStat < ApplicationRecord
   # ['idme', 'myhealthevet', 'dslogon']
   LOGIN_TYPES = SAML::User::AUTHN_CONTEXTS.map { |_k, v| v[:sign_in][:service_name] }.uniq.freeze
