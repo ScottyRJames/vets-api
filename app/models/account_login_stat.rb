90f9442f455b773aeebd116595e1155e268ac08e
Josh Scanish <josh.scanish@oddball.io>
2021-08-26 17:16:46 -0400

    Find correct constants

--- app/models/account_login_stat.rb
+++ app/models/account_login_stat.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class AccountLoginStat < ApplicationRecord
-  LOGIN_TYPES = %w[idme dslogon mhv].freeze
+  # ['idme', 'myhealthevet', 'dslogon']
+  LOGIN_TYPES = SAML::User::AUTHN_CONTEXTS.map { |_k, v| v[:sign_in][:service_name] }.uniq.freeze
 
   belongs_to :account, inverse_of: :login_stats
   validates :account_id, presence: true, uniqueness: true
