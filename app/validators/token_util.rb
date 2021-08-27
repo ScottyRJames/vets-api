7eb1b21f8f7476711119789d159ff92d338fa3af
Derek Brown <derek.brown@libertyits.com>
2021-08-27 14:50:04 -0400

    Removed unused method for the time being.

--- app/validators/token_util.rb
+++ app/validators/token_util.rb
@@ -3,10 +3,9 @@
 class TokenUtil
   def self.validate_token(token)
     raise error_klass('Invalid audience') unless TokenUtil.valid_audience?(token)
-    return true if token.static?
-    raise error_klass('Invalid issuer') unless TokenUtil.valid_issuer?(token)
 
-    true
+    # Only static tokens utilize this validator at this time
+    token.static?
   end
 
   # Validates the token audience against the service caller supplied `aud` payload.
@@ -18,14 +17,6 @@ class TokenUtil
       # Temorarily accept the default audience or the API specificed audience
       [Settings.oidc.isolated_audience.default, *token.aud].include?(token.payload['aud'])
     end
-  end
-
-  # Validates the token issuer is a valid issuer configured in the Settings.oidc list.
-  def self.valid_issuer?(token)
-    iss = token.payload['iss'] if token.payload
-    !iss.nil? && iss.match?(%r{^#{Regexp.escape(Settings.oidc.issuer_prefix)}/\w+$})
-  rescue e
-    raise error_klass(e.message)
   end
 
   def self.error_klass(error_detail_string)
